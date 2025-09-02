1b8f9107b91719f98d7e5f4e513b164e7f925b95
aveA <aveA@AVE>
2025-09-03 00:26:55 +0300

    add base to gallerypage

---
+++ PicApp/PicApp/PicApp/Models/GalleryImage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace PicApp.Models
+{
+    public class GalleryImage
+    {
+        public Guid Guid { get; set; }
+
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        private string url;
+        public string Url
+        {
+            get => url;
+            set
+            {
+                if (url != value)
+                {
+                    url = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        public GalleryImage(string name = null, string url = null)
+        {
+            Guid = Guid.NewGuid();
+            this.name = name;
+            this.url = url;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
--- PicApp/PicApp/PicApp/Pages/GalleryPage.xaml.cs
+++ PicApp/PicApp/PicApp/Pages/GalleryPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PicApp.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,34 @@ namespace PicApp.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class GalleryPage : ContentPage
     {
+        public ObservableCollection<GalleryImage> Images { get; set; } = new ObservableCollection<GalleryImage>();
+
         public GalleryPage()
         {
             InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            Images = new ObservableCollection<GalleryImage>() 
+            { 
+                new GalleryImage("name1", "maxresdefault.jpg"),
+                new GalleryImage("name2", "maxresdefault.jpg"),
+                new GalleryImage("name3", "maxresdefault.jpg"),
+            };
+
+            BindingContext = this;
+            base.OnAppearing();
+        }
+
+        private void OnClickedOpenImage(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnClickedDeleteImage(object sender, EventArgs e)
+        {
+
         }
     }
 }
